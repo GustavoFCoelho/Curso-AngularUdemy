baa9ef4c0754772b1fd8e983d17ad4ffc1f2e159
GustavoFCoelho <gustavo.f.coelho09@gmail.com>
2020-06-10 16:31:52 -0300

    CrudFirebase

--- 07-CRUDFireBase/src/app/pages/heroe/heroe.component.ts
+++ 07-CRUDFireBase/src/app/pages/heroe/heroe.component.ts
@@ -1,6 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from "../../model/hero";
 import {NgForm} from "@angular/forms";
+import {HeroService} from "../../service/hero.service";
+import Swal from "sweetalert2";
+import {Observable} from "rxjs";
+import {ActivatedRoute} from "@angular/router";
+
 
 @Component({
   selector: 'app-heroe',
@@ -11,18 +16,43 @@ export class HeroeComponent implements OnInit {
 
   hero: Hero = new Hero();
 
-  constructor() {
+  constructor(private service: HeroService, private router:ActivatedRoute) {
   }
 
   ngOnInit() {
+    const id = this.router.snapshot.paramMap.get("id");
+    if(id !== "nuevo"){
+      this.service.getHero(id).subscribe((result:Hero)=>{
+        this.hero = result;
+        this.hero.id = id;
+      })
+    }
   }
 
   guardar(f: NgForm) {
-    if(f.invalid){
-      alert("Formulario não valido")
+    if (f.invalid) {
+      alert("Formulario não valido");
       return;
     }
-    console.log(f);
-    console.log(this.hero);
+    Swal.fire({
+      title: "Aguarde",
+      text: "Salvando a Informação",
+      allowOutsideClick: false,
+    });
+    Swal.showLoading();
+
+    let peticao: Observable<any>;
+    if (this.hero.id) {
+      peticao = this.service.atualizarHero(this.hero);
+    } else {
+      peticao = this.service.criarHero(this.hero)
+    }
+
+    peticao.subscribe((result:any)=>{
+      Swal.fire({
+        title: this.hero.nombre,
+        text: "Foi salvo com sucesso",
+      })
+    })
   }
 }
--- 07-CRUDFireBase/src/app/pages/heroes/heroes.component.ts
+++ 07-CRUDFireBase/src/app/pages/heroes/heroes.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HeroService} from "../../service/hero.service";
+import {Hero} from "../../model/hero";
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes: Hero[];
+  carregando = false;
+
+  constructor(private service: HeroService) {
+  }
 
   ngOnInit() {
+    this.carregando = true;
+    this.service.getHeros().subscribe((result) => {
+      this.heroes = result;
+      this.carregando = false;
+    })
+  }
+
+  thanos(id: string, i: number) {
+    Swal.fire({
+      title: "Deseja Excluir?",
+      showConfirmButton: true,
+      showCancelButton: true
+    }).then(resp => {
+      if (resp.value) {
+        this.service.blip(id).subscribe((result) => {
+          console.log(result);
+          this.heroes.splice(i, 1);
+        });
+      }
+    });
+
   }
 
 }
